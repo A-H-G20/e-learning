c97941490d1a8d692b4c89f9d58ba9c8fc05669b
Ahmad Ghosen <ahmadghosen20@gmail.com>
2025-04-28 16:03:15 +0300

    done

--- signIn.php
+++ signIn.php
@@ -135,7 +135,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             // Redirect based on user role
             if ($user['role'] == 'admin') {
-                header('Location: admin/index.php');
+                header('Location: AdminHtml/index.php');
             } elseif ($user['role'] == 'user' || $user['role'] == 'deaf' || $user['role'] == 'blind') {
                 header('Location: index.php');
             } elseif ($user['role'] == 'instructor') {
