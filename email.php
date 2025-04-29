3ddc86ae40fe7d8667d1d7c1aadbe7f0be486783
Ahmad Ghosen <ahmadghosen20@gmail.com>
2025-04-29 09:40:23 +0300

    edit

--- email.php
+++ email.php
@@ -1,4 +1,4 @@
 <?php
 
-$mail->Username = 'ahmadghosen20@gmail.com'; // Replace with your email
-$mail->Password = 'bbievwnemblpxuqt'; // Replace with your password or app password
+$mail->Username = 'email@gmail.com'; // Replace with your email
+$mail->Password = 'password'; // Replace with your password or app password
